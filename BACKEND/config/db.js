edc63f7ddc379bba7e5085fe239bd5a31144e3e6
Yogesh <me.yogesh.pal@gmail.com>
2024-07-22 17:07:00 +0530

    updated changes

--- BACKEND/config/db.js
+++ BACKEND/config/db.js
@@ -4,7 +4,6 @@ const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGODB_URI);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
-        alert("database connected");
     } catch (error) {
         console.error(`Error: ${error.message}`);
         process.exit(1);
--- BACKEND/index.js
+++ BACKEND/index.js
@@ -16,8 +16,8 @@ app.use(express.json({ extended: false }));
 
 //app.use(cors());
 app.use(cors({
-    origin: "https://make-my-trip-clone-ecru.vercel.app", // Replace with your React app's domain
-// origin : "http://localhost:3000",
+  //  origin: "https://make-my-trip-clone-ecru.vercel.app", // Replace with your React app's domain
+ origin : "http://localhost:3000",
     credentials: true // Allow cookies for authenticated requests (if applicable)
   }));
 
