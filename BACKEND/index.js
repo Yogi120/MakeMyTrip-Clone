7512b6ab6f64c37632e7610ab13dd82e77e73ece
Yogesh Pal <120707604+Yogi120@users.noreply.github.com>
2024-07-22 23:48:40 +0530

    Update index.js

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -14,12 +14,12 @@ const start = async () => {
 
 app.use(express.json({ extended: false }));
 
-app.use(cors());
-//app.use(cors({
+//app.use(cors());
+app.use(cors({
   //  origin: "https://make-my-trip-clone-ecru.vercel.app", // Replace with your React app's domain
- //origin : "https://make-my-trip-clone-gamma.vercel.app",
-   // credentials: true // Allow cookies for authenticated requests (if applicable)
-  //}));
+ origin : "https://make-my-trip-clone-frontend.vercel.app",
+    credentials: true // Allow cookies for authenticated requests (if applicable)
+  }));
 
 // Define Routes
 app.get('/',(req,res)=>{
