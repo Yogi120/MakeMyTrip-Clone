a42663df7d6f6e2570a36d522f6851bca437e4ef
Yogesh <me.yogesh.pal@gmail.com>
2024-07-26 12:48:09 +0530

    files updates

--- BACKEND/config/config.js
+++ BACKEND/config/config.js
@@ -3,6 +3,4 @@
 module.exports = {
     jwtSecret: process.env.JWT_SECRET || 'MyScretKey',  // JWT secret key
     mongoURI: process.env.MONGODB_URI || 'mongodb://0.0.0.0:27017/MakeMyTripDB',  // MongoDB connection URI
-    // Add more configuration variables as needed
-    //'mongodb://0.0.0.0:27017/MakeMyTripDB'
 };
--- BACKEND/index.js
+++ BACKEND/index.js
@@ -1,37 +1,47 @@
 const express = require('express');
-const connectDB = require('./config/db');
 const dotenv = require('dotenv');
 const cors = require('cors');
-const hotelBookingRoutes = require('./routes/hotelBookingRoutes');
+const connectDB = require('./config/db');
+
 dotenv.config();
+
 const port = process.env.PORT || 5000;
 const app = express();
 
+const authRoutes = require('./routes/auth');
+const flightRoutes = require('./routes/flightRoutes');
+const hotelRoutes = require('./routes/hotelRoutes');
+const countryRoutes = require('./routes/countryRoutes');
+const searchFlightRoutes = require('./routes/searchFlightRoutes');
+const searchHotelRoutes = require('./routes/searchHotelRoutes');
+const bookingRoutes = require('./routes/bookingRoutes');
+const hotelBookingRoutes = require('./routes/hotelBookingRoutes');
+
 const start = async () => {
     await connectDB();
-    app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+    app.listen(port, () => console.log(`Server running on port ${port}!`));
 };
 
 app.use(express.json({ extended: false }));
 
-//app.use(cors());
 app.use(cors({
-  //  origin: "https://make-my-trip-clone-ecru.vercel.app", // Replace with your React app's domain
- origin : "https://make-my-trip-clone-frontend.vercel.app",
+    origin: "https://make-my-trip-clone-frontend.vercel.app", // Replace with your React app's domain
     credentials: true // Allow cookies for authenticated requests (if applicable)
-  }));
+}));
 
 // Define Routes
-app.get('/',(req,res)=>{
-  res.send('server running')
-})
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/admin/flights', require('./routes/flightRoutes'));
-app.use('/api/admin/hotels', require('./routes/hotelRoutes'));
-app.use('/api/getallcountry', require('./routes/countryRoutes'));
-app.use('/api/searchFlight', require('./routes/searchFlightRoutes'));
-app.use('/api/searchHotel', require('./routes/searchHotelRoutes'));
-app.use('/api', require('./routes/bookingRoutes'));
+app.get('/', (req, res) => {
+    res.send('Server running');
+});
+app.use('/api/auth', authRoutes);
+app.use('/api/admin/flights', flightRoutes);
+app.use('/api/admin/hotels', hotelRoutes);
+app.use('/api/getallcountry', countryRoutes);
+app.use('/api/searchFlight', searchFlightRoutes);
+app.use('/api/searchHotel', searchHotelRoutes);
+app.use('/api', bookingRoutes);
 app.use('/api', hotelBookingRoutes);
+
 start();
+
 module.exports = app;
