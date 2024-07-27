2a69ecbdef45ff6c756373380bca8b093eee2e65
Yogesh <me.yogesh.pal@gmail.com>
2024-07-27 23:55:41 +0530

    final commit

--- BACKEND/utile/CountryCities.js
+++ BACKEND/utile/CountryCities.js
@@ -1,334 +1,184 @@
-const CountryCities =[
-    {
-      "country": "India",
-      "cities": [
-        {
-          "cityName": "Delhi",
-          "airportCode": "DEL",
-          "airportName": "Indira Gandhi International Airport"
-        },
-        {
-          "cityName": "Mumbai",
-          "airportCode": "BOM",
-          "airportName": "Chhatrapati Shivaji Maharaj International Airport"
-        },
-        {
-          "cityName": "Bangalore",
-          "airportCode": "BLR",
-          "airportName": "Kempegowda International Airport"
-        },
-        {
-          "cityName": "Chennai",
-          "airportCode": "MAA",
-          "airportName": "Chennai International Airport"
-        },
-        {
-          "cityName": "Kolkata",
-          "airportCode": "CCU",
-          "airportName": "Netaji Subhas Chandra Bose International Airport"
-        }
-      ]
-    },
-    {
-      "country": "USA",
-      "cities": [
-        {
-          "cityName": "New York",
-          "airportCode": "JFK",
-          "airportName": "John F. Kennedy International Airport"
-        },
-        {
-          "cityName": "Los Angeles",
-          "airportCode": "LAX",
-          "airportName": "Los Angeles International Airport"
-        },
-        {
-          "cityName": "Chicago",
-          "airportCode": "ORD",
-          "airportName": "O'Hare International Airport"
-        },
-        {
-          "cityName": "San Francisco",
-          "airportCode": "SFO",
-          "airportName": "San Francisco International Airport"
-        },
-        {
-          "cityName": "Miami",
-          "airportCode": "MIA",
-          "airportName": "Miami International Airport"
-        }
-      ]
-    },
-    {
-      "country": "UK",
-      "cities": [
-        {
-          "cityName": "London",
-          "airportCode": "LHR",
-          "airportName": "Heathrow Airport"
-        },
-        {
-          "cityName": "Manchester",
-          "airportCode": "MAN",
-          "airportName": "Manchester Airport"
-        },
-        {
-          "cityName": "Edinburgh",
-          "airportCode": "EDI",
-          "airportName": "Edinburgh Airport"
-        },
-        {
-          "cityName": "Birmingham",
-          "airportCode": "BHX",
-          "airportName": "Birmingham Airport"
-        },
-        {
-          "cityName": "Glasgow",
-          "airportCode": "GLA",
-          "airportName": "Glasgow Airport"
-        }
-      ]
-    },
-    {
-      "country": "Australia",
-      "cities": [
-        {
-          "cityName": "Sydney",
-          "airportCode": "SYD",
-          "airportName": "Sydney Kingsford Smith Airport"
-        },
-        {
-          "cityName": "Melbourne",
-          "airportCode": "MEL",
-          "airportName": "Melbourne Airport"
-        },
-        {
-          "cityName": "Brisbane",
-          "airportCode": "BNE",
-          "airportName": "Brisbane Airport"
-        },
-        {
-          "cityName": "Perth",
-          "airportCode": "PER",
-          "airportName": "Perth Airport"
-        },
-        {
-          "cityName": "Adelaide",
-          "airportCode": "ADL",
-          "airportName": "Adelaide Airport"
-        }
-      ]
-    },
-    {
-      "country": "Canada",
-      "cities": [
-        {
-          "cityName": "Toronto",
-          "airportCode": "YYZ",
-          "airportName": "Toronto Pearson International Airport"
-        },
-        {
-          "cityName": "Vancouver",
-          "airportCode": "YVR",
-          "airportName": "Vancouver International Airport"
-        },
-        {
-          "cityName": "Montreal",
-          "airportCode": "YUL",
-          "airportName": "Montréal-Pierre Elliott Trudeau International Airport"
-        },
-        {
-          "cityName": "Calgary",
-          "airportCode": "YYC",
-          "airportName": "Calgary International Airport"
-        },
-        {
-          "cityName": "Ottawa",
-          "airportCode": "YOW",
-          "airportName": "Ottawa Macdonald-Cartier International Airport"
-        }
-      ]
-    },
-    {
-      "country": "Germany",
-      "cities": [
-        {
-          "cityName": "Berlin",
-          "airportCode": "BER",
-          "airportName": "Berlin Brandenburg Airport"
-        },
-        {
-          "cityName": "Frankfurt",
-          "airportCode": "FRA",
-          "airportName": "Frankfurt Airport"
-        },
-        {
-          "cityName": "Munich",
-          "airportCode": "MUC",
-          "airportName": "Munich Airport"
-        },
-        {
-          "cityName": "Hamburg",
-          "airportCode": "HAM",
-          "airportName": "Hamburg Airport"
-        },
-        {
-          "cityName": "Düsseldorf",
-          "airportCode": "DUS",
-          "airportName": "Düsseldorf Airport"
-        }
-      ]
-    },
-    {
-      "country": "Japan",
-      "cities": [
-        {
-          "cityName": "Tokyo",
-          "airportCode": "HND",
-          "airportName": "Haneda Airport"
-        },
-        {
-          "cityName": "Osaka",
-          "airportCode": "KIX",
-          "airportName": "Kansai International Airport"
-        },
-        {
-          "cityName": "Nagoya",
-          "airportCode": "NGO",
-          "airportName": "Chubu Centrair International Airport"
-        },
-        {
-          "cityName": "Fukuoka",
-          "airportCode": "FUK",
-          "airportName": "Fukuoka Airport"
-        },
-        {
-          "cityName": "Sapporo",
-          "airportCode": "CTS",
-          "airportName": "New Chitose Airport"
-        }
-      ]
-    },
-    {
-      "country": "China",
-      "cities": [
-        {
-          "cityName": "Beijing",
-          "airportCode": "PEK",
-          "airportName": "Beijing Capital International Airport"
-        },
-        {
-          "cityName": "Shanghai",
-          "airportCode": "PVG",
-          "airportName": "Shanghai Pudong International Airport"
-        },
-        {
-          "cityName": "Guangzhou",
-          "airportCode": "CAN",
-          "airportName": "Guangzhou Baiyun International Airport"
-        },
-        {
-          "cityName": "Shenzhen",
-          "airportCode": "SZX",
-          "airportName": "Shenzhen Bao'an International Airport"
-        },
-        {
-          "cityName": "Chengdu",
-          "airportCode": "CTU",
-          "airportName": "Chengdu Shuangliu International Airport"
-        }
-      ]
-    },
-    {
-      "country": "Brazil",
-      "cities": [
-        {
-          "cityName": "Sao Paulo",
-          "airportCode": "GRU",
-          "airportName": "São Paulo-Guarulhos International Airport"
-        },
-        {
-          "cityName": "Rio de Janeiro",
-          "airportCode": "GIG",
-          "airportName": "Rio de Janeiro-Galeão International Airport"
-        },
-        {
-          "cityName": "Brasilia",
-          "airportCode": "BSB",
-          "airportName": "Brasilia International Airport"
-        },
-        {
-          "cityName": "Salvador",
-          "airportCode": "SSA",
-          "airportName": "Deputado Luís Eduardo Magalhães International Airport"
-        },
-        {
-          "cityName": "Recife",
-          "airportCode": "REC",
-          "airportName": "Recife/Guararapes–Gilberto Freyre International Airport"
-        }
-      ]
-    },
-    {
-      "country": "South Korea",
-      "cities": [
-        {
-          "cityName": "Seoul",
-          "airportCode": "ICN",
-          "airportName": "Incheon International Airport"
-        },
-        {
-          "cityName": "Busan",
-          "airportCode": "PUS",
-          "airportName": "Gimhae International Airport"
-        },
-        {
-          "cityName": "Jeju",
-          "airportCode": "CJU",
-          "airportName": "Jeju International Airport"
-        },
-        {
-          "cityName": "Daegu",
-          "airportCode": "TAE",
-          "airportName": "Daegu International Airport"
-        },
-        {
-          "cityName": "Gwangju",
-          "airportCode": "KWJ",
-          "airportName": "Gwangju Airport"
-        }
-      ]
-    },
-    {
-      "country": "South Africa",
-      "cities": [
-        {
-          "cityName": "Johannesburg",
-          "airportCode": "JNB",
-          "airportName": "O. R. Tambo International Airport"
-        },
-        {
-          "cityName": "Cape Town",
-          "airportCode": "CPT",
-          "airportName": "Cape Town International Airport"
-        },
-        {
-          "cityName": "Durban",
-          "airportCode": "DUR",
-          "airportName": "King Shaka International Airport"
-        },
-        {
-          "cityName": "Port Elizabeth",
-          "airportCode": "PLZ",
-          "airportName": "Chief Dawid Stuurman International Airport"
-        },
-        {
-          "cityName": "Bloemfontein",
-          "airportCode": "BFN",
-          "airportName": "Bram Fischer International Airport"
-        }
-      ]
-    }
-  ]
-  
-  module.exports = CountryCities;
+const CountryCities = [
+  {
+    "country": "Spain",
+    "cities": [
+      {
+        "cityName": "Madrid",
+        "airportCode": "MAD",
+        "airportName": "Adolfo Suárez Madrid-Barajas Airport"
+      },
+      {
+        "cityName": "Barcelona",
+        "airportCode": "BCN",
+        "airportName": "Barcelona-El Prat Airport"
+      },
+      {
+        "cityName": "Valencia",
+        "airportCode": "VLC",
+        "airportName": "Valencia Airport"
+      },
+      {
+        "cityName": "Seville",
+        "airportCode": "SVQ",
+        "airportName": "Seville Airport"
+      },
+      {
+        "cityName": "Bilbao",
+        "airportCode": "BIO",
+        "airportName": "Bilbao Airport"
+      }
+    ]
+  },
+  {
+    "country": "France",
+    "cities": [
+      {
+        "cityName": "Paris",
+        "airportCode": "CDG",
+        "airportName": "Charles de Gaulle Airport"
+      },
+      {
+        "cityName": "Nice",
+        "airportCode": "NCE",
+        "airportName": "Nice Côte d'Azur Airport"
+      },
+      {
+        "cityName": "Lyon",
+        "airportCode": "LYS",
+        "airportName": "Lyon-Saint Exupéry Airport"
+      },
+      {
+        "cityName": "Marseille",
+        "airportCode": "MRS",
+        "airportName": "Marseille Provence Airport"
+      },
+      {
+        "cityName": "Toulouse",
+        "airportCode": "TLS",
+        "airportName": "Toulouse-Blagnac Airport"
+      }
+    ]
+  },
+  {
+    "country": "Italy",
+    "cities": [
+      {
+        "cityName": "Rome",
+        "airportCode": "FCO",
+        "airportName": "Leonardo da Vinci International Airport"
+      },
+      {
+        "cityName": "Milan",
+        "airportCode": "MXP",
+        "airportName": "Milan Malpensa Airport"
+      },
+      {
+        "cityName": "Venice",
+        "airportCode": "VCE",
+        "airportName": "Venice Marco Polo Airport"
+      },
+      {
+        "cityName": "Florence",
+        "airportCode": "FLR",
+        "airportName": "Florence Airport"
+      },
+      {
+        "cityName": "Naples",
+        "airportCode": "NAP",
+        "airportName": "Naples International Airport"
+      }
+    ]
+  },
+  {
+    "country": "Netherlands",
+    "cities": [
+      {
+        "cityName": "Amsterdam",
+        "airportCode": "AMS",
+        "airportName": "Amsterdam Schiphol Airport"
+      },
+      {
+        "cityName": "Rotterdam",
+        "airportCode": "RTM",
+        "airportName": "Rotterdam The Hague Airport"
+      },
+      {
+        "cityName": "Eindhoven",
+        "airportCode": "EIN",
+        "airportName": "Eindhoven Airport"
+      },
+      {
+        "cityName": "Maastricht",
+        "airportCode": "MST",
+        "airportName": "Maastricht Aachen Airport"
+      },
+      {
+        "cityName": "Groningen",
+        "airportCode": "GRQ",
+        "airportName": "Groningen Eelde Airport"
+      }
+    ]
+  },
+  {
+    "country": "Sweden",
+    "cities": [
+      {
+        "cityName": "Stockholm",
+        "airportCode": "ARN",
+        "airportName": "Stockholm Arlanda Airport"
+      },
+      {
+        "cityName": "Gothenburg",
+        "airportCode": "GOT",
+        "airportName": "Göteborg Landvetter Airport"
+      },
+      {
+        "cityName": "Malmö",
+        "airportCode": "MMX",
+        "airportName": "Malmö Airport"
+      },
+      {
+        "cityName": "Uppsala",
+        "airportCode": "UPP",
+        "airportName": "Uppsala Airport"
+      },
+      {
+        "cityName": "Visby",
+        "airportCode": "VBY",
+        "airportName": "Visby Airport"
+      }
+    ]
+  },
+  {
+    "country": "Denmark",
+    "cities": [
+      {
+        "cityName": "Copenhagen",
+        "airportCode": "CPH",
+        "airportName": "Copenhagen Airport"
+      },
+      {
+        "cityName": "Aarhus",
+        "airportCode": "AAR",
+        "airportName": "Aarhus Airport"
+      },
+      {
+        "cityName": "Billund",
+        "airportCode": "BLL",
+        "airportName": "Billund Airport"
+      },
+      {
+        "cityName": "Aalborg",
+        "airportCode": "AAL",
+        "airportName": "Aalborg Airport"
+      },
+      {
+        "cityName": "Karup",
+        "airportCode": "KRP",
+        "airportName": "Karup Airport"
+      }
+    ]
+  }
+];
+
+module.exports = CountryCities;
